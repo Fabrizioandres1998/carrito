2c09d199212bd2343ac4bbdf2e1d7d73de70ff37
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-10-30 00:32:54 -0300

    reset

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const contenedor = document.querySelector(".producto")
-const nombreProducto = contenedor.querySelector("h3")
-const precioProducto = contenedor.querySelector("p")
-const boton = contenedor.querySelector("button")
-const carrito = document.querySelector(".carro")
-const list = carrito.querySelector("ul")
-const totalPrecio = carrito.querySelector("p")
+//QUIERO HACER UN CARRITO DE COMPRAS QUE SIGA GUARDADO AUN CUANDO HAGA RELOAD
+//PASO 1 OBTENER LOS ELEMENTOS NECESARIOS DEL HTML PARA INTERACTUAR CON EL DOM
+//PASO 2 CREAR OBJETO DE PRODUCTOS
+//PASO 3 CREAR UN EVENTO QUE AL HACER CLICK EN EL BOTON SE AGREGUE AL CARRITO EL PRODUCTO Y EL PRECIO
+//CARRITO DEBERIA SER UN ARRAY
+//GUARDAR EN LOCALSTORAGE LOS OBJETOS O ARRAYS NECESARIOS PARA OBTENER LOS DATOS DEL CARRITO DESDE ALLI
 
-let objetoProducto = {
-    nombre: nombreProducto.textContent,
-    precio: precioProducto.textContent
+const boton = document.querySelector("button")
+const ul = document.querySelector("ul")
+const total = document.querySelector(".total")
+
+let producto1 = {
+    nombre: "Madera tirantes",
+    precio: 5000
 }
 
-boton.addEventListener("click", () => {
-    totalPrecio.textContent = "TOTAL " + objetoProducto.precio
-    let carritoActual =  []
-    carritoActual.push(objetoProducto)
-    localStorage.setItem("carritoDeCompras", JSON.stringify(objetoProducto))
-    // nuevoLi.textContent = objetoProducto.nombre
-    // let nuevoLi = document.createElement("li")
-    // carrito.appendChild(nuevoLi)
-})
+let array = []
 
+boton.addEventListener("click", () => {
+    let totalActual = parseFloat(total.textContent);
+    
+    // Sumar el precio del producto al total actual
+    totalActual += producto1.precio;
+
+    // Asignar el nuevo total al contenido de total.textContent
+    total.textContent = totalActual;
+    let nuevoLi = document.createElement("li")
+    nuevoLi.textContent = "Madera tirantes"
+    ul.appendChild(nuevoLi)
+})
